a16ccded84dece46ed19c8bdec5f680a1728154f
Hector <hector@example.com>
2026-04-23 08:20:14 -0500

    feat(autor): vistas función editar y eliminar

--- gestion/views.py
+++ gestion/views.py
@@ -18,3 +18,21 @@ def crear_autor(request):
     else:
         form = AutorForm()
     return render(request, 'gestion/autor_form.html', {'form': form})
+
+def editar_autor(request, pk):
+    autor = get_object_or_404(Autor, pk=pk)
+    if request.method == 'POST':
+        form = AutorForm(request.POST, instance=autor)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_autores')
+    else:
+        form = AutorForm(instance=autor)
+    return render(request, 'gestion/autor_form.html', {'form': form})
+
+def eliminar_autor(request, pk):
+    autor = get_object_or_404(Autor, pk=pk)
+    if request.method == 'POST':
+        autor.delete()
+        return redirect('lista_autores')
+    return render(request, 'gestion/autor_confirm_delete.html', {'autor': autor})
