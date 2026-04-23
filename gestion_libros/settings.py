c177a489455789f23472a1c464e66a7eee8f8621
Hector <hector@example.com>
2026-04-23 09:42:47 -0500

    feat: improve all templates with professional design

--- gestion_libros/settings.py
+++ gestion_libros/settings.py
@@ -73,12 +73,23 @@ WSGI_APPLICATION = 'gestion_libros.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+
+# Para Vercel, usar base de datos en memoria
+if os.environ.get('VERCEL'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
