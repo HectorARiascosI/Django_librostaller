4d9f09b50fd8631db0179670a0df92d83ca8f296
Hector <hector@example.com>
2026-04-23 08:19:43 -0500

    feat(autor): vistas función lista y crear

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Autor
+from .forms import AutorForm
 
-# Create your views here.
+# VISTAS BASADAS EN FUNCIONES (4)
+def lista_autores(request):
+    autores = Autor.objects.all()
+    return render(request, 'gestion/lista_autores.html', {'autores': autores})
+
+def crear_autor(request):
+    if request.method == 'POST':
+        form = AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_autores')
+    else:
+        form = AutorForm()
+    return render(request, 'gestion/autor_form.html', {'form': form})
